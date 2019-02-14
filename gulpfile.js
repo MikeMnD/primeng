15713914fd77810c0f14caddf8e56d2e6416ad50
mihail_ilinov <m.ilinov@blubito.com>
2019-02-14 13:53:45 +0200

    Fixes to simulate the real primeng structure on publish

--- gulpfile.js
+++ gulpfile.js
@@ -7,55 +7,63 @@ var gulp = require('gulp'),
     del = require('del'),
     flatten = require('gulp-flatten');
 
-gulp.task('build-css', function() {
-	gulp.src([
+gulp.task('build-css', function () {
+    gulp.src([
         'src/app/components/common/common.css',
-		    'src/app/components/**/*.css'
+        'src/app/components/**/*.css'
     ])
         .pipe(concat('primeng.css'))
         .pipe(gulp.dest('dist/resources'));
 });
 
-gulp.task('build-css-prod', function() {
+gulp.task('build-css-prod', function () {
     gulp.src([
         'src/app/components/common/common.css',
         'src/app/components/**/*.css'
     ])
-    .pipe(concat('primeng.css'))
-    .pipe(gulp.dest('dist/resources'))
-    .pipe(uglifycss({"uglyComments": true}))
-    .pipe(rename('primeng.min.css'))
-    .pipe(gulp.dest('dist/resources'));
+        .pipe(concat('primeng.css'))
+        .pipe(gulp.dest('dist/resources'))
+        .pipe(uglifycss({"uglyComments": true}))
+        .pipe(rename('primeng.min.css'))
+        .pipe(gulp.dest('dist/resources'));
 });
 
 gulp.task('copy-component-css', function () {
     gulp.src([
         'src/app/components/**/*.css'
     ])
-    .pipe(gulp.dest('dist/resources/components'));
+        .pipe(gulp.dest('dist/resources/components'));
 });
 
-gulp.task('images', function() {
+gulp.task('images', function () {
     return gulp.src(['src/app/components/**/images/*.png', 'src/app/components/**/images/*.gif'])
         .pipe(flatten())
         .pipe(gulp.dest('dist/resources/images'));
 });
 
-gulp.task('themes', function() {
+gulp.task('themes', function () {
     return gulp.src(['src/assets/components/themes/**/*'])
         .pipe(gulp.dest('dist/resources/themes'));
 });
 
-gulp.task('build-exports', function() {
-    return gulp.src(['exports/*.js','exports/*.d.ts'])
+gulp.task('build-exports', function () {
+    return gulp.src(['exports/*.js', 'exports/*.d.ts'])
         .pipe(gulp.dest('./dist/'));
 });
 
+gulp.task('copy-package-json', function () {
+    gulp.src([
+        'package.json'
+    ])
+        .pipe(gulp.dest('./dist/'));
+});
+
+
 //Cleaning previous gulp tasks from project
-gulp.task('clean', function() {
-	del(['dist/resources']);
+gulp.task('clean', function () {
+    del(['./dist/resources']);
 });
 
 //Building project with run sequence
-gulp.task('build-assets', ['clean','copy-component-css', 'build-css-prod', 'images', 'themes']);
+gulp.task('build-assets', ['clean', 'copy-component-css', 'copy-package-json', 'build-css-prod', 'images', 'themes']);
 
